17d2774c7533c10325d602a9ad5aad411071fb68
eugenenguyen <xuanbinh91@gmail.com>
2015-01-23 08:54:07 +0700

    Initial commit

---
+++ Example/Pods/Target Support Files/Pods-XBGallery/Pods-XBGallery-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// XBGallery
+#define COCOAPODS_POD_AVAILABLE_XBGallery
+#define COCOAPODS_VERSION_MAJOR_XBGallery 0
+#define COCOAPODS_VERSION_MINOR_XBGallery 1
+#define COCOAPODS_VERSION_PATCH_XBGallery 0
+
